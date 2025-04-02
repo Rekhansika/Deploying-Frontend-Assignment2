8a11b15e59cc56e32f588ea4bf4c24ed490fb228
Rekhansika <rekhagoluguri02@gmail.com>
2025-04-02 14:29:38 +0530

    frontend

---
+++ Frontend/src/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <h2>Goluguri Rekhansika</h2>
+      <h3>goluguri.rekhansika.s89@kalvium.community</h3>
+    </div>
+  )
+}
+
+export default Home
